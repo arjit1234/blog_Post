21cdf5edba356ba35d3d2a192ad6b02e58bc2beb
arjit1234 <lit.arjitbiswall@gmail.com>
2021-01-26 16:11:21 +0530

    Hello

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,27 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from blog.models import Post
 
 
-posts=[
-	{
-		'author':'Arjit Biswal',
-		'title':'Blog Post 1',
-		'content':'First post content',
-		'date_posted':'August 27,2018',
-
-	},
-	{
-		'author':'Suresh',
-		'title':'Blog Post 2',
-		'content':'Second Post content',
-		'date_posted':'August 28,2018',
-	}
-]
 
 def home(request):
 	context={
-		'posts':posts
+		'posts':Post.objects.all()
 	}
 	return render(request,"blog/home.html",context)
 
